ec6d0a79f52cdf35dc2477e7cd967ec8bec0c31e
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-12-15 10:52:29 +0530

    Leetcode 746 - Min Cost to climb stairs

--- DSA/Dynamic Programming/6.MinCostClimbStairs.cpp
+++ DSA/Dynamic Programming/6.MinCostClimbStairs.cpp
@@ -1,9 +1,68 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
+// Recursion
+// int solve(vector<int>& cost, int n)
+// {
+//     if(n==0 || n==1)
+//     return cost[n];
+
+//     return (cost[n] + min(solve(cost, n-1), solve(cost, n-2)));
+// }
+
+// int minCostClimbingStairs(vector<int>& cost)
+// {
+//     int n = cost.size();
+//     return min(solve(cost, n-1), solve(cost, n-2));
+// }
+
+// Recursion + Memoization
+int solve(vector<int>& cost, int n, vector<int>& dp)
+{
+    if(n==0 || n==1)
+    return cost[n];
+
+    if(dp[n] != -1)
+    return dp[n];
+
+    dp[n] = cost[n] + min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+    return dp[n];
+}
+
+// Tabulation
+int solve2(vector<int>& cost, int n)
+{
+    vector<int> dp(n+1);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for(int i=2 ; i<n ; i++)
+    {
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+    }
+
+    return min(dp[n-1], dp[n-2]);
+}
+
+int minCostClimbingStairs(vector<int>& cost)
+{
+    int n = cost.size();
+    vector<int> dp (n+1, -1);
+    return min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+}
+
 int main()
 {
+    vector<int> data = {10,15,20};
+    int res = minCostClimbingStairs(data);
+    cout<<"Stairs: ";
+    for(int i: data)
+    cout<<i<<" ";
+
+    cout<<"\nMinimum Cost to climb stairs: "<<res<<endl;
+
     return 0;
 }
 
