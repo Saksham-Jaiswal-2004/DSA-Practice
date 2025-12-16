22f6c75cba3e5cfe4b17ac6554cb0d169a5d5e39
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-12-17 02:25:04 +0530

    Code Fix

--- Leetcode/Dynamic Programming/5.ClimbStairs.cpp
+++ Leetcode/Dynamic Programming/5.ClimbStairs.cpp
@@ -36,6 +36,29 @@ int main()
     return 0;
 }
 
+// Leetcode
+// 70. Climbing Stairs
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+// Coding Ninjas
 // Problem statement
 // You have been given a number of stairs. Initially, you are at the 0th stair, 
 // and you need to reach the Nth stair.
